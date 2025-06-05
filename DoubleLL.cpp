204ba1d84f5328060a8a4deedc1e8a69da16e5ca
Ayush Mehta <ayushmehtabps@gmail.com>
2025-06-05 16:32:05 +0530

    add-doubleLL

---
+++ DoubleLL.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Define the Node structure
+struct Node {
+    public:
+    int data;
+    Node* next;
+    Node*back;
+
+    // Constructor with two parameters
+    Node(int data1, Node* next1,Node*back1) {
+        data = data1;
+        next = next1;
+        back=back1;
+    }
+
+    // Constructor with only data, sets next to nullptr
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+        back=nullptr;
+    }
+
+};
+Node*convertarr2DLL(vector<int>&arr){
+    Node*head= new Node(arr[0]);
+    Node*prev= head;
+    for(int i=1;i<arr.size();i++){
+        Node*temp= new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+
+}
+void print(Node*head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+int main(){
+      vector<int> arr = {2, 5, 9, 11};
+
+    Node* head = convertarr2DLL(arr);
+    print(head);
+}
